20eff75e2c80d4edf9e27babadf0d10d08f941bf
chenpengyyds <2668864300@qq.com>
2026-03-24 10:44:01 +0800

    Update fastAPI.py

--- fastAPI.py
+++ fastAPI.py
@@ -106,9 +106,4 @@ async def get_cif(mpid: str):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)))
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)    # <--- 只留一个右括号
